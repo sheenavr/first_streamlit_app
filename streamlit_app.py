cf9b1b6da99346f6ec36dc2d9e42f3e4d47bf5ea
sheenavr <vrsheenavr@gmail.com>
2023-12-13 15:28:31 -0500

    Update streamlit_app.py
    read file using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('🥣Breakfast Menu')
@@ -8,4 +9,7 @@ streamlit.text('🥑 Avacado toast')
 streamlit.text('🐔 Hard boiled free-range egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
