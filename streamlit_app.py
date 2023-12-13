7b37d36cda0570c3ffaf178d3a4551ed0e56de24
sheenavr <vrsheenavr@gmail.com>
2023-12-13 15:19:23 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oat Meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard boiled free-range egg')
