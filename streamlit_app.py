4dac5c8f68a07950c19ce48231f5f647c0f82fe2
sheenavr <vrsheenavr@gmail.com>
2023-12-13 15:23:52 -0500

    Update streamlit_app.py
    added  images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oat Meal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega3 & Blueberry Oat Meal')
+streamlit.text('🍞 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 Avacado toast')
+streamlit.text('🐔 Hard boiled free-range egg')
+
